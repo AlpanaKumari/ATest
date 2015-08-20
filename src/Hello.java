bcb6edd707c610e25b43546d83d3ad98a9b69b7c
alpana.kumari <alpana.kumari@BLRKEC263928D.ad.infosys.com>
2015-08-20 12:56:32 +0530

    Changed done.

---
+++ src/Hello.java
@@ -0,0 +1,32 @@
+
+
+import java.io.*;
+import java.util.logging.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/Hello")
+public class Hello extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public Hello() {
+		Logger.getLogger("inside constructor...............");
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		out.print("<html><body>");
+		out.print("<h3>Hello Servlet<h3>");
+		out.print("</body></html>");
+		
+	}
+
+}
